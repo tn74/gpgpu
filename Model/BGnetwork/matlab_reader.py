90ee8e11db5c496228ac9789537a09ed971d2238
Trishul Nagenalli <tn74@duke.edu>
2019-05-02 20:12:17 -0400

    Pulling updated data

--- Model/BGnetwork/matlab_reader.py
+++ Model/BGnetwork/matlab_reader.py
@@ -1,5 +1,26 @@
 from scipy.io import loadmat
 import os
+from shutil import copytree
 
-def save_directory(out_folder_name):
-    
+
+def save_matlab(test_name):
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    out_path =os.path.join(basepath, "saved_tests", test_name)
+    copytree(os.path.join(basepath, "saveddata"), out_path)
+
+
+def load_test_data(test_name):
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    test_path = os.path.join(basepath, "saved_tests", test_name)
+
+    for mat_fname in os.listdir(test_path):
+        mat_path = os.path.join(test_path, mat_fname)
+        if ".mat" not in mat_path:
+            continue
+        data = loadmat(mat_path)
+        print(data)
+
+
+if __name__ == "__main__":
+    # save_matlab("test1")
+    load_test_data(("test1"))
