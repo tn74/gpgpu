d4940f88237d6a211935e76f1797c117bffea472
Trishul Nagenalli <tn74@duke.edu>
2019-09-11 17:13:04 -0400

    read arbitrary data into struct from txt file

---
+++ pycall/CMakeFiles/pycall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pycall.dir/main.cpp.o"
+  "pycall.pdb"
+  "pycall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pycall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pycall/main.py
@@ -0,0 +1,3 @@
+import ctypes
+
+NUM = 16
