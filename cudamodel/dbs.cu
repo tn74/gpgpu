9c039007daa8e4950af5a5f7b9e51ec3ad92212b
Trishul <trishul.nagenalli@duke.edu>
2019-11-28 13:23:23 -0500

    computer array sum faster on gpu than cpu

--- cudamodel/dbs.cu
+++ cudamodel/dbs.cu
@@ -2,8 +2,55 @@
 // Created by Trishul Nagenalli on 2019-11-21.
 //
 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 #include "src/BGNetwork.h"
 
+__global__
+void compute_preceeding(int* arr, int* sumarr, int maxlen) {
+    int maxind = 64 * blockIdx.x + threadIdx.x;
+    if (maxind >= maxlen) {
+        return;
+    }
+    sumarr[maxind] = 0;
+    for (int i = 0; i < maxind; ++i) {
+        sumarr[maxind] += arr[i];
+    } 
+}
+
+extern "C" int cudatest() {
+    int arrlen = 90000;
+    int* arr = (int*) malloc(sizeof(int) * arrlen);
+    int* sumarr = (int*) malloc(sizeof(int) * arrlen); 
+    cudaMallocManaged(&arr, arrlen*sizeof(int));
+    cudaMallocManaged(&sumarr, arrlen*sizeof(int));
+    for (int i = 0; i < arrlen; ++i) {
+        arr[i] = i;   
+        sumarr[i] = 0;
+    }
+    for (int i = 0; i < arrlen; ++i) {
+        std::cout << sumarr[i] << " ";    
+    }
+    std::cout << std::endl;
+    std::cout << "Past Init" << std::endl;
+    //compute_preceeding<<<arrlen/64 + 1, 64>>>(arr, sumarr, arrlen);
+    
+    for (int tix = 0; tix < arrlen; ++tix) {
+        for (int i = 0; i < tix; ++i) {
+            sumarr[tix] += arr[i];
+        }
+    }
+    
+    std::cout << "Past Trap" << std::endl;
+    cudaDeviceSynchronize();
+    for (int i = 0; i < arrlen; ++i) {
+        std::cout << sumarr[i] << " ";    
+    }
+    std::cout << std::endl;
+    return 0;
+}
 
 extern "C" int plain() {
     return 1;
