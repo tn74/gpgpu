2e8af20a93d3bb6629325dc06d301162f3cb9b0a
Trishul <trishul.nagenalli@duke.edu>
2019-11-30 09:26:39 -0500

    Added first python test of run

--- cudamodel/test.cu
+++ cudamodel/test.cu
@@ -1,12 +1,10 @@
 #include <iostream>
-
-//#include "src/gating.h"
 #include "src/BGNetwork.h"
 #include "dbs.cu"
-int main() {
-    
+
+extern "C" int test_run() { 
     auto sim_params = (simulation_parameters_t *) malloc(sizeof(simulation_parameters));
-    sim_params->cells_per_type = 1000;
+    sim_params->cells_per_type = 10;
     sim_params->duration = 1;
     sim_params->dt = 0.01;
     return execute_simulation(sim_params);
