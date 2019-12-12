e630375ec403145b0ee4ec57d08279972b3f78d6
Trishul <trishul.nagenalli@duke.edu>
2019-12-12 13:15:26 -0500

    Reverted 6 commits

--- cudamodel/main.cu
+++ cudamodel/main.cu
@@ -9,9 +9,9 @@
 int main() { 
     auto sim_params = (simulation_parameters_t *) malloc(sizeof(simulation_parameters));
     sim_params->cells_per_type = 10;
-    sim_params->duration = 10;
+    sim_params->duration = 1;
     sim_params->dt = 0.01;
-    auto result = execute_simulation_debug(sim_params);
+    execute_simulation_debug(sim_params);
     std::cout<< "End of main " << std::endl;
     return 0;
 }
