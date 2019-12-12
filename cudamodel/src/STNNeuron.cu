a42270fa0246ee76e0197868f8a05e4f5cefce8d
Trishul <trishul.nagenalli@duke.edu>
2019-12-11 22:14:23 -0500

    Added STN

---
+++ cudamodel/src/STNNeuron.cu
@@ -0,0 +1,45 @@
+//
+// Created by Jasmine Lu on 2019-04-27.
+//
+
+#include "STNNeuron.h"
+#include "gating.h"
+
+
+__device__ void compute_next_states(stn_state_t *in, stn_state_t *out, stn_param_t *params, double dt){
+    compute_currents(in, out, params);
+    compute_gating(in, out, params, dt);
+    
+    double current_sum =
+            out -> I_L +
+            out -> I_Na +
+            out -> I_K +
+            out -> I_T +
+            out -> I_Ca +
+            out -> I_ahp;
+    out -> voltage = in -> voltage + dt * current_sum / params->C_m;
+}
+
+__device__ void compute_currents(stn_state_t *in, stn_state_t *out, stn_param_t *params){
+    out->I_L = -(params->g_L * (in->voltage - params->E_L));
+    out->I_Na = -(params->g_Na * (pow(stn_minf(in->voltage), 3)) * in->H * (in->voltage- params->E_Na));
+    out->I_K = -(params->g_K * pow(in->N, 4) * (in->voltage- params->E_K));
+    out->I_T = -(params->g_T * pow(stn_ainf(in->voltage), 3) * pow(stn_binf(in->R), 2) * in->R * (in->voltage- params->E_Ca));
+    out->I_Ca = -(params->g_Ca * pow(in->C, 2) * (in->voltage- params->E_Ca));
+    out->I_ahp = -(params->g_ahp * (in->voltage- params->E_ahp) * (in->CA/(in->CA + 15)));
+}
+__device__ void compute_gating(stn_state_t *in, stn_state_t *out, stn_param_t *params, double dt){
+    out->H = in->H + dt * 0.75 * ((stn_hinf(in->voltage) - in->H)/stn_tauh(in->voltage));
+    out->R = in->R + dt * 0.2 * (stn_rinf(in->voltage) - in->R)/stn_taur(in->voltage);
+    out->N = in->N + dt * 0.75 * (stn_ninf(in->voltage) - in->N)/stn_taun(in->voltage);
+    out->C = in->C + dt * 0.08 * (stn_cinf(in->voltage) - in->C)/stn_tauc(in->voltage);
+    out->CA = in->CA + dt * 3.75 * pow(10, -5) * (-(in->I_Ca) - in->I_T - (22.5 * in->CA));
+}
+void init_state(stn_state_t *in){
+    in->voltage = -57.0;
+    in->H = stn_hinf(in->voltage);
+    in->R = stn_rinf(in->voltage);
+    in->N = stn_ninf(in->voltage);
+    in->C = stn_cinf(in->voltage);
+    in->CA = 0.1;
+}
---
+++ cudamodel/src/STNNeuron.h
@@ -0,0 +1,46 @@
+//
+// Created by Jasmine Lu on 2019-04-27.
+//
+
+#ifndef DBS_ON_GPU_STNNEURON_H
+#define DBS_ON_GPU_STNNEURON_H
+
+
+typedef struct stn_param {
+    double C_m;
+    double g_L;
+    double E_L;
+    double g_Na;
+    double E_Na;
+    double g_K;
+    double E_K;
+    double g_T;
+    double E_T;
+    double g_Ca;
+    double E_Ca;
+    double g_ahp;
+    double E_ahp;
+} stn_param_t;
+
+
+typedef struct stn_state {
+    double voltage;
+    double I_Na;
+    double I_L;
+    double I_K;
+    double I_T;
+    double I_Ca;
+    double I_ahp;
+    double H;
+    double R;
+    double N;
+    double C;
+    double CA;
+} stn_state_t;
+ 
+__device__ void compute_next_states(stn_state_t *in, stn_state_t *out, stn_param_t *params, double dt);
+__device__ void compute_currents(stn_state_t *in, stn_state_t *out, stn_param_t *params);
+__device__ void compute_gating(stn_state_t *in, stn_state_t *out, stn_param_t *params, double dt);
+void init_state(stn_state_t *in);
+
+#endif //DBS_ON_GPU_STNNEURON_H
